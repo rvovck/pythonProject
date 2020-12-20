2e69419655a15107d9a2176de9880de090eee200
PavloKonoplov <38275433+PavloKonoplov@users.noreply.github.com>
2020-12-20 21:47:27 +0200

    main api

--- check_models.py
+++ check_models.py
@@ -1,22 +1,26 @@
 from models import *
+from sqlalchemy.orm import sessionmaker
+from db_credentials import *
 
-
+engine = create_engine(DATABASE_CONNECTION)
+Session = sessionmaker(bind=engine)
 session = Session()
 
+user1 = User(userId=1, username='user_20', password='hgp880d', role=2)
+user2 = User(userId=2, username='user_21', password='hgp881d', role=1)
 
-user = User(uid=1, username='user_20', password='hgp880d', role=2)
-user2 = User(uid=2, username='user_21', password='hgp880d', role=2)
-city = City(cid=1, name='Lviv')
-ad = AD(adid=1, title='empty', content='empty', from_user=user, from_city=city)
-ad1 = AD(adid=2, title='empty', content='empty', from_user=user2, from_city=city)
-ad2 = AD(adid=3, title='empty', content='empty', from_user=user2, from_city=city)
+city1 = City(cityId=1, cityname='Lviv')
 
+ad1 = Ad(title='empty', content='empty', from_user=user1, from_city=city1)
+ad2 = Ad(title='empty', content='empty', from_user=user2, from_city=city1)
 
-session.add(user)
+session.add(user1)
 session.add(user2)
-session.add(city)
-session.add(ad)
+session.commit()
+
+session.add(city1)
+session.commit()
+
 session.add(ad1)
 session.add(ad2)
-
 session.commit()
